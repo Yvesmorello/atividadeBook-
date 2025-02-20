b34e23d907befde726d53947c57608b7ce6ffe70
Yvesmorello <124888523+Yvesmorello@users.noreply.github.com>
2025-02-20 20:22:45 -0300

    Add files via upload

---
+++ atividadeBook/src/main/java/com/example/atividadeBook/service/BookService.java
@@ -0,0 +1,34 @@
+package com.example.atividadeBook.service;
+
+import com.example.atividadeBook.repository.BookRepository;
+import com.example.atividadeBook.domain.Book;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+
+    public Book create(Book book){
+        Book createdBook = bookRepository.save(book);
+        return createdBook;
+    };
+
+    public Book findById(Long id){
+        Book foundBook = bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Not Found"));
+        return foundBook;
+    };
+
+    public Book update(Long id, Book book){
+        Book updatedbook = bookRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Not found"));
+
+        return bookRepository.save(book);
+    };
+
+    public void delete(Long id){
+        bookRepository.deleteById(id);
+    };
+}
